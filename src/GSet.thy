fa3b57228697bf13c50ff97bae497aa040be398e
Taylor Blau <me@ttaylorr.com>
2020-05-24 13:04:39 -0600

    src: generalize GSet to non-delta version

--- src/GSet.thy
+++ src/GSet.thy
@@ -10,16 +10,16 @@ type_synonym ('a) operation = "'a state"
 fun insert :: "'a \<Rightarrow> ('a state) \<Rightarrow> ('a operation)" where
 "insert a as = { a }"
 
-fun delta_gset_op :: "('a operation) \<Rightarrow> ('a state) \<rightharpoonup> ('a state)" where
-"delta_gset_op a as = Some ( as \<union> a )"
+fun gset_op :: "('a operation) \<Rightarrow> ('a state) \<rightharpoonup> ('a state)" where
+"gset_op a as = Some ( as \<union> a )"
 
-locale delta_gset = network_with_ops _ delta_gset_op "{}"
+locale gset = network_with_ops _ gset_op "{}"
 
-lemma (in delta_gset) [simp] : "delta_gset_op x \<rhd> delta_gset_op y = delta_gset_op y \<rhd> delta_gset_op x"
+lemma (in gset) [simp] : "gset_op x \<rhd> gset_op y = gset_op y \<rhd> gset_op x"
   apply (auto simp add: kleisli_def)
 done
 
-lemma (in delta_gset) concurrent_operations_commute:
+lemma (in gset) concurrent_operations_commute:
   assumes "xs prefix of i"
   shows "hb.concurrent_ops_commute (node_deliver_messages xs)"
   using assms
@@ -27,7 +27,7 @@ lemma (in delta_gset) concurrent_operations_commute:
   apply(unfold interp_msg_def, simp)
 done
 
-corollary (in delta_gset) set_convergence:
+corollary (in gset) set_convergence:
   assumes "set (node_deliver_messages xs) = set (node_deliver_messages ys)"
       and "xs prefix of i"
       and "ys prefix of j"
@@ -35,13 +35,13 @@ corollary (in delta_gset) set_convergence:
 using assms by(auto simp add: apply_operations_def intro: hb.convergence_ext concurrent_operations_commute
                 node_deliver_messages_distinct hb_consistent_prefix)
 
-context delta_gset begin
+context gset begin
 
 sublocale sec: strong_eventual_consistency weak_hb hb interp_msg
   "\<lambda>ops. \<exists>xs i. xs prefix of i \<and> node_deliver_messages xs = ops" "{}"
   apply(standard; clarsimp)
       apply(auto simp add: hb_consistent_prefix drop_last_message node_deliver_messages_distinct concurrent_operations_commute)
-   apply(metis (full_types) interp_msg_def delta_gset_op.elims)
+   apply(metis (full_types) interp_msg_def gset_op.elims)
   using drop_last_message apply blast
   done
 end
